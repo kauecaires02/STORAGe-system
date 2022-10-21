9293ce54d106c60f157be3e2e8a7a1eceb32101e
_kaueuak <84874622+kauecaires02@users.noreply.github.com>
2022-10-21 04:00:46 -0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,23 @@
+# system
+
+#ENTRADA DE DADOS
+cpf = input("Digite o cpf: ")
+nome = input ("Digite o nome: ")
+código = input ("Digite o cód: ")
+qtde = input ("Digite a qntde: ")
+valor = input ("Digite o valor: ")
+
+print ("--------------------------------")
+
+print ("    >>>Dados do pedido")
+
+print ("--------------------------------")
+
+#SAÍDA DE DADOS
+print ("CPF informado: ") - cpf
+print ("NOME informado: ") - nome
+print ("CÓDIGO informado: ") - codigo
+print ("QUANTIDADE informado: ") - qtde
+print ("VALOR informado: ") - valor
+
+print ("--------------------------------")
